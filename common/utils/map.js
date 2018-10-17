340b096add714f0ecbcca60802e561e181d64867
Hugo Da Roit <contact@hdaroit.fr>
2018-10-17 19:10:56 +0200

    add map service

---
+++ common/utils/map.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reverseLocation(lat, lng) {
+    // TODO : ask openstreetmap API to convert lat/lng to an address
+    // module : request, npm install request request-promise-native --save, const request = require('request-promise-native');
+    // const res = await request.get({json: {lat, lng}});
+  },
+};
