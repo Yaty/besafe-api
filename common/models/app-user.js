0418a681d48059084202c98819d27b2b96105b99
Hugo Da Roit <contact@hdaroit.fr>
2018-10-17 18:03:31 +0200

    Initial commit

---
+++ common/models/app-user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(AppUser) {
+  AppUser.observe('before save', async function(ctx) {
+    if (ctx.instance && ctx.instance.phone) {
+      ctx.instance.username = ctx.instance.phone;
+    } else if (ctx.data && ctx.data.phone) {
+      ctx.data.username = ctx.data.phone;
+    }
+  });
+
+  AppUser.beforeRemote('login', async function(ctx) {
+    ctx.args.credentials.username = ctx.args.credentials.phone;
+    delete ctx.args.credentials.phone;
+  });
+};
