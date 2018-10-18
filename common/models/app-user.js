bbb0e3f51e70fbe9a88c8e48f33e8c8f7740f6e8
Angelo Deliessche <angelo.deliessche@gmail.com>
2018-10-18 10:52:46 +0200

    Reverse localisation (map.js; app-user.js)

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -1,4 +1,5 @@
 'use strict';
+const map = require ('../utils/map.js');
 
 module.exports = function(AppUser) {
   AppUser.validatesUniquenessOf('phone');
@@ -14,5 +15,11 @@ module.exports = function(AppUser) {
   AppUser.beforeRemote('login', async function(ctx) {
     ctx.args.credentials.username = ctx.args.credentials.phone;
     delete ctx.args.credentials.phone;
+  });
+
+  AppUser.afterRemote('prototype.__create__alerts', async function(ctx) {
+
+    const addresse = await map.reverseLocation(ctx.result.location);
+
   });
 };
--- common/utils/map.js
+++ common/utils/map.js
@@ -1,18 +1,25 @@
-const request = require("request");
+const request = require("request-promise-native");
 
 
 module.exports = {
-  reverseLocation(lat, lng) {
+  async reverseLocation({lat, lng}) {
     // TODO : ask openstreetmap API to convert lat/lng to an address
     // module : request, npm install request request-promise-native --save, const request = require('request-promise-native');
     // const res = await request.get({json: {lat, lng}});
 
-		const url = "https://nominatim.openstreetmap.org/reverse?format=json&lat="+lat+"&lon="+lng;
-		request.get(url, (error, response, body) => {
-		  let json = JSON.parse(body);
-		  const adresse = json.adresse.road();
-		  console.log(adresse);
-		});
+		const body = await request.get({
+      headers: {
+        referer: 'hdaroit.fr'
+      },
+      url: 'https://nominatim.openstreetmap.org/reverse',
+      qs: {
+        format: 'json',
+        lat,
+        lon: lng,
+      },
+      json : true,
+    });
+    return body.display_name;
     //https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-34.44076&lon=-58.70521
   },
 };
